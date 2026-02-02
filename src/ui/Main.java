64e053eab2ca84a3f3cc1d85816da96449282b3d
Javiera Puga <ttaxyy@proton.me>
2026-02-02 19:40:11 -0300

    Agrega threads y ExecutorService

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -2,40 +2,59 @@ package ui;
 
 import model.*;
 
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
     public static void main(String[] args) {
         Direccion direccion1 = new Direccion("Valparaíso", "Viña del Mar", "14 Norte", 1200);
-        Repartidor repartidor1 = new Repartidor("Benjamín Gómez", true);
-        Repartidor repartidor2 = new Repartidor("Rodrigo Castro", false);
 
-        Pedido[] pedidos = {
-           new PedidoComida(1, direccion1, 15, repartidor1),
-           new PedidoEncomienda(2, direccion1, 15, repartidor1,40, true),
-           new PedidoExpress(3, direccion1, 15, repartidor1),
-           new PedidoComida(4, direccion1, 7, repartidor2),
-           new PedidoEncomienda(5, direccion1, 13, repartidor2,20, false),
-           new PedidoExpress(6, direccion1, 15, repartidor2),
+        ArrayList<Pedido> pedidos1 = new ArrayList<>() {
+            {   add(new PedidoComida(1, direccion1, 15));
+                add(new PedidoEncomienda(2, direccion1, 15,40, true));
+                add(new PedidoExpress(3, direccion1, 15));
+            }
         };
 
-        for (Pedido p : pedidos) {
-            System.out.println("------------------------------------------------");
-            System.out.println("Pedido recibido.");
-            p.asignarRepartidor();
-            System.out.println("-----");
-            System.out.println("Ingresando manualmente repartidor...");
+        ArrayList<Pedido> pedidos2 = new ArrayList<>() {
+            {   add(new PedidoComida(4, direccion1, 7));
+                add(new PedidoEncomienda(5, direccion1, 13,20, false));
+                add(new PedidoExpress(6, direccion1, 15));
+            }
+        };
 
-            String nombreRepartidor = p.getRepartidor().getNombreRepartidor();
-            p.asignarRepartidor(nombreRepartidor);
-            System.out.println("Información del pedido:");
-            p.mostrarResumen();
-            System.out.println("-----");
-            p.calcularTiempoEntrega();
-            p.despachar();
-            p.cancelar();
-            p.registrarEntrega();
+        ArrayList<Pedido> pedidos3 = new ArrayList<>() {
+            {   add(new PedidoComida(7, direccion1, 16));
+                add(new PedidoEncomienda(8, direccion1, 12,15, false));
+                add(new PedidoExpress(9, direccion1, 4));
+            }
+        };
+
+        /* Repartidor[] repartidores = {
+                new Repartidor ("Benjamín Gómez", true, pedidos1),
+                new Repartidor ("Rodrigo Castro", false, pedidos2),
+                new Repartidor("Sofía Morales", true, pedidos3),
+        };
+
+
+        for (Repartidor r : repartidores) {
+            Thread thread = new Thread(r);
+            thread.start();
+        } */
+
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        executor.execute(new Repartidor ("Benjamín Gómez", true, pedidos1));
+        executor.execute(new Repartidor ("Rodrigo Castro", false, pedidos2));
+        executor.execute(new Repartidor("Sofía Morales", true, pedidos3));
+
+        try {
+            Thread.sleep(15000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
-        System.out.println("------------------------------------------------");
-        System.out.println("Historial de todos los pedidos:");
-        pedidos[0].verHistorial();
+
+        executor.shutdownNow();
     }
 }
