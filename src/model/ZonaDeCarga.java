eeb29fc484209cb31ee61d89d4fa06565684a0cf
Javiera Puga <ttaxyy@proton.me>
2026-02-28 22:05:28 -0300

    Actualiza zona de carga

--- src/model/ZonaDeCarga.java
+++ src/model/ZonaDeCarga.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -10,17 +12,29 @@ public class ZonaDeCarga {
         this.colaPedidos = new LinkedBlockingQueue<>();
     }
 
-    public synchronized void agregarPedido(Pedido p) throws InterruptedException {
-        colaPedidos.put(p); //TODO: Cambiar a try/catch para manejar exception
-    };
+    public void agregarPedido(Pedido p) throws InterruptedException {
+        if (p == null) throw new IllegalArgumentException("El pedido no puede ser nulo");
+        colaPedidos.put(p);
+        System.out.println("Pedido " + p.getIdPedido() + " agregado a la zona de carga.");
+    }
 
-    public synchronized Pedido retirarPedido() {
-        Pedido pedido = colaPedidos.poll(); //poll() => Retira el primer elemento de la cola y lo devuelve.
-
+    public Pedido retirarPedido() {
+        Pedido pedido = colaPedidos.poll();
         if (pedido != null) {
-            System.out.println("Pedido " + pedido.getIdPedido() + " retirado de la zona de carga");
+            System.out.println("Pedido " + pedido.getIdPedido() + " retirado de la zona de carga.");
         }
-
         return pedido;
+    }
+
+    public List<Pedido> listarPedidos() { //Convierte el queue en lista
+        return new ArrayList<>(colaPedidos);
+    }
+
+    public boolean existePedido(int id) { //Revisar si existe ID (ya que se ingresa manual en panel)
+        return colaPedidos.stream().anyMatch(p -> p.getIdPedido() == id);
+    }
+
+    public boolean estaVacia() {
+        return colaPedidos.isEmpty();
     }
 }
